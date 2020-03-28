162c635ead2157d2b02877d3b9c6e3bad8f17417
katdom13 <katdom13@gmail.com>
2020-03-28 13:05:10 +0800

    Migrate from test db sqlite to mysql

--- app/config/config.py
+++ app/config/config.py
@@ -6,6 +6,7 @@ from flask import Flask
 game = Flask(__name__, template_folder='../view', static_folder='../static')
 game.config['SECRET_KEY'] = os.urandom(24)
 game.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-game.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../record.db'
+# game.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../record.db'
+game.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/record'
 
 db = SQLAlchemy(game)
